84bfa76d3dc85a7fdda0b7a15ebb4be7c81e205e
Brandon Powers <brandonkpowers@gmail.com>
2017-01-07 22:45:09 -0500

    Created Twitter class to abstract twitter functions in pshare

---
+++ twitter.py
@@ -0,0 +1,26 @@
+'''
+Object representing the actions specified 
+with the -t flag for Twitter
+'''
+
+class Twitter:
+    def __init__(self):
+        print 'Twitter: __init__()'
+
+    def auth():
+        print 'Twitter: auth()'
+    
+    def verify():
+        print 'Twitter: verify()'
+    
+    def status_to_tweets(statuses):
+        print 'Twitter: statuses_to_tweets()'
+
+    def read():
+        print 'Twitter: read()'
+
+    def post():
+        print 'Twitter: post()'
+
+    def del():
+        print 'Twitter: del()'
