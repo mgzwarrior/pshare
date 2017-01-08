3475cd0275169e2ff541a5b3a534f0f32879e990
Brandon Powers <brandonkpowers@gmail.com>
2017-01-08 15:19:08 -0500

    Finished implementing Twitter class

--- pshare.py
+++ pshare.py
@@ -1,23 +1,13 @@
 '''
 Authors: Brandon Powers & Matt Grant
 '''
-from tweet import Tweet
-import sys
+
+from twitter import Twitter
 import argparse
-import tweepy
-import os.path
 
 # Disables two warnings when pshare is run
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
-
-# initializing authentication -- consumer info is hidden in local directory, not on GitHub for security purposes -- ask for use
-TWITTER_CONSUMER_KEY = ''
-TWITTER_CONSUMER_SECRET = ''
-auth = tweepy.OAuthHandler(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET)
-
-TWITTER_ACCESS_KEY = ''
-TWITTER_ACCESS_SECRET = ''
 
 # initializing parser 
 # usage: psh <flags> <command> <cargs | cflags>
@@ -44,103 +34,6 @@ cflags.add_argument('-s', '--status', type=str, default='',
                     help='name of text file to be posted')
 args = parser.parse_args()
 
-def init_twitter():
-    try:
-        filename = 'twitter-access-token.txt'
-        print '**Initial authentication required for first-time user**'
-        print '(1) Open the following URL in a web browser: ' + str(auth.get_authorization_url())
-        print '(2) Authorize pshare to post to + read from your Twitter account'
-        verifier = raw_input('(3) Input the access code Twitter redirects you to here: ')
-        print '(4) Getting access token...'
-        auth.get_access_token(verifier)
-        print '(5) Storing access token information in file "twitter-access-token.txt" for future use'
-        with open(filename, 'w') as outfile:
-            outfile.write('{}\n{}'.format(auth.access_token, auth.access_token_secret))
-    except tweepy.TweepError:
-        print 'pshare.py: error: failed to get request token OR access token.'
-    
-def auth_twitter():
-    # first line is TWITTER_ACCESS_KEY, second line is TWITTER_ACCESS_SECRET <-- reason for hardcoded indices
-    filename = 'twitter-access-token.txt'
-    with open(filename, 'r') as infile:
-        access_token = infile.readlines()
-        TWITTER_ACCESS_KEY = str(access_token[0]).rstrip('\n')
-        TWITTER_ACCESS_SECRET = str(access_token[1])
-    auth.set_access_token(TWITTER_ACCESS_KEY, TWITTER_ACCESS_SECRET)
-
-def verify_twitter(api):
-    try:
-        if api.verify_credentials():
-            print 'Verified authentication credentials -- gained access to Twitter API'
-    except tweepy.TweepError:
-        print 'pshare.py: error: credentials are invalid: delete "twitter-access-token.txt" & re-authenticate'
-        sys.exit(1)
-
-def statuses_to_tweets(statuses):
-    tweets = []
-    for status in statuses:
-        name = str(status.user.name)
-        screen_name = str(status.user.screen_name)
-        text = status.text
-        created_at = status.created_at
-        id_num = status.id
-        tweets.append(Tweet(name, screen_name, text, created_at, id_num))
-    return tweets
-
-def read_twitter(api):
-    statuses = []
-    if args.cargs == 'home':
-        statuses = api.home_timeline(count=args.number)
-    elif args.cargs == 'user':
-        statuses = api.user_timeline(count=args.number)
-    tweets = statuses_to_tweets(statuses)
-    print '\n**Top {} tweets on your timeline**\n'.format(str(args.number))
-    for tweet in tweets:
-        tweet.display(args.verbose)
-
-def post_twitter(api):
-    try:
-        # tweet media with cargs as text | tweet cargs as text, no media
-        if not args.cargs == 'home' and args.media:
-            api.update_with_media(args.media, args.cargs)
-        elif not args.cargs == 'home':
-            api.update_status(args.cargs)
-
-        # tweet media with status file contents as text
-        elif args.media and args.status:
-            status = ''
-            with open(args.status, 'r') as infile:
-                status = infile.read()
-            api.update_with_media(args.media, status) 
-
-        # only media file -- tweet media
-        elif args.media:
-            api.update_with_media(args.media)
-
-        # only status file -- tweet contents
-        elif args.status:
-            status = ''
-            with open(args.status, 'r') as infile:
-                status = infile.read()
-            api.update_status(status)
-
-        # prompt user -- no cargs, media, or status file
-        else:
-            status = raw_input('Enter tweet to post: ')
-            api.update_status(status)
-    
-    except tweepy.TweepError:
-        print 'pshare.py: error: tweet was over 140 characters OR encountered a post error' 
-        sys.exit(1)
-
-def del_twitter(api):
-    try:
-        api.destroy_status(args.cargs)
-        print 'Deleted tweet with id #: {}'.format(str(args.cargs))
-    except tweepy.TweepError:
-        print 'pshare.py: error: tweet id # is invalid OR delete failed' 
-        sys.exit(1)
-
 def init_facebook():
     print 'init_facebook()'
 
@@ -152,27 +45,19 @@ def verify_facebook():
 
 def main():
     if args.twitter:
-        filename = 'twitter-access-token.txt'
-        access_token_exists = os.path.isfile(filename)
-        if access_token_exists:
-            auth_twitter()
-        else:
-            init_twitter()
-        
-        print 'Authenticating credentials...'
-        api = tweepy.API(auth)
-        verify_twitter(api)
-        
+        t = Twitter(args)
+        t.login()
+        t.get_tweepy_API()
         if args.command == 'read':
-            read_twitter(api)
+            t.read()
         elif args.command == 'post':
-            post_twitter(api)
+            t.post()
         else:
-            del_twitter(api)
-    
+            t.delete()
+
     if args.facebook:
-        # Facebook OAuth not implemented yet
-        print 'called with -f flag for facebook'
+        # TODO: implement Facebook class OR functions
+        print 'Facebook flag -f specified'
     
 if __name__ == '__main__':
     main()
--- twitter.py
+++ twitter.py
@@ -3,24 +3,126 @@ Object representing the actions specified
 with the -t flag for Twitter
 '''
 
+from tweet import Tweet
+import tweepy
+import os.path
+
+# initializing authentication -- consumer info is hidden in local directory, not on GitHub for security purposes -- ask for use
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
+
 class Twitter:
-    def __init__(self):
-        print 'Twitter: __init__()'
+    def __init__(self, args):
+        self.auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+        self.filename = 'twitter-access-token.txt'
+        self.api = ''
+        self.args = args
+        
+    def login(self):
+        access_token_exists = os.path.isfile(self.filename)
+        if access_token_exists: 
+            self.known_user_auth()
+        else:
+            self.first_time_auth()
 
-    def auth():
-        print 'Twitter: auth()'
+    def known_user_auth(self):
+        # first line is TWITTER_ACCESS_KEY, second line is TWITTER_ACCESS_SECRET <-- reason for hardcoded indices
+        with open(self.filename, 'r') as infile:
+            access_token = infile.readlines()
+            ACCESS_KEY = str(access_token[0]).rstrip('\n')
+            ACCESS_SECRET = str(access_token[1])
+        self.auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+    def first_time_auth(self):
+        try:
+            print '**Initial authentication required for first-time user**'
+            print '(1) Open the following URL in a web browser: ' + str(self.auth.get_authorization_url())
+            print '(2) Authorize pshare to post to + read from your Twitter account'
+            verifier = raw_input('(3) Input the access code Twitter redirects you to here: ')
+            print '(4) Getting access token...'
+            self.auth.get_access_token(verifier)
+            print '(5) Storing access token information in file "twitter-access-token.txt" for future use'
+            with open(self.filename, 'w') as outfile:
+                outfile.write('{}\n{}'.format(self.auth.access_token, self.auth.access_token_secret))
+        except tweepy.TweepError:
+            print 'pshare.py: error: failed to get request token OR access token.'
+
+    def get_tweepy_API(self):
+        print 'Authenticating credentials...'
+        self.api = tweepy.API(self.auth)
+        self.verify()
+
+    def verify(self):
+        try:
+            if self.api.verify_credentials():
+                print 'Verified authentication credentials -- gained access to Twitter API'
+        except tweepy.TweepError:
+            print 'pshare.py: error: credentials are invalid: delete "twitter-access-token.txt" & re-authenticate'
+            sys.exit(1)
+
+    def read(self):
+        statuses = []
+        if self.args.cargs == 'home':
+            statuses = self.api.home_timeline(count=self.args.number)
+        elif self.args.cargs == 'user':
+            statuses = self.api.user_timeline(count=self.args.number)
+        tweets = self.statuses_to_tweets(statuses)
+        print '\n**Top {} tweets on your timeline**\n'.format(str(self.args.number))
+        for tweet in tweets:
+            tweet.display(self.args.verbose)
     
-    def verify():
-        print 'Twitter: verify()'
-    
-    def status_to_tweets(statuses):
-        print 'Twitter: statuses_to_tweets()'
+    def statuses_to_tweets(self, statuses):
+        tweets = []
+        for status in statuses:
+            name = str(status.user.name)
+            screen_name = str(status.user.screen_name)
+            text = status.text
+            created_at = status.created_at
+            id_num = status.id
+            tweets.append(Tweet(name, screen_name, text, created_at, id_num))
+        return tweets
 
-    def read():
-        print 'Twitter: read()'
+    def post(self):
+        try:
+            # tweet media with cargs as text | tweet cargs as text, no media
+            if not self.args.cargs == 'home' and self.args.media:
+                self.api.update_with_media(self.args.media, self.args.cargs)
+            elif not self.args.cargs == 'home':
+                self.api.update_status(self.args.cargs)
 
-    def post():
-        print 'Twitter: post()'
+            # tweet media with status file contents as text
+            elif self.args.media and self.args.status:
+                status = ''
+                with open(self.args.status, 'r') as infile:
+                    status = infile.read()
+                self.api.update_with_media(self.args.media, status) 
 
-    def del():
-        print 'Twitter: del()'
+            # only media file -- tweet media
+            elif self.args.media:
+                self.api.update_with_media(args.media)
+
+            # only status file -- tweet contents
+            elif self.args.status:
+                status = ''
+                with open(self.args.status, 'r') as infile:
+                    status = infile.read()
+                self.api.update_status(status)
+
+            # prompt user -- no cargs, media, or status file
+            else:
+                status = raw_input('Enter tweet to post: ')
+                self.api.update_status(status)
+        
+        except tweepy.TweepError:
+            print 'pshare.py: error: tweet was over 140 characters OR encountered a post error' 
+            sys.exit(1)
+
+    def delete(self):
+        try:
+            self.api.destroy_status(self.args.cargs)
+            print 'Deleted tweet with id #: {}'.format(str(self.args.cargs))
+        except tweepy.TweepError:
+            print 'pshare.py: error: tweet id # is invalid OR delete failed' 
+            sys.exit(1)
