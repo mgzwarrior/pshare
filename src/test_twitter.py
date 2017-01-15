61522548e7659f41875c08c9d5c744a56020a554
Brandon Powers <brandonkpowers@gmail.com>
2017-01-15 17:35:28 -0500

    Worked on Twitter unit tests

--- src/test_twitter.py
+++ src/test_twitter.py
@@ -6,9 +6,18 @@ Purpose:
 '''
 
 import unittest
+from twitter import Twitter
+from argparse import Namespace
+
+# Disables two warnings when pshare is run
+import requests.packages.urllib3
+requests.packages.urllib3.disable_warnings()
 
 class TestTwitter(unittest.TestCase):
     def setUp(self):
+        # simulate "psh -t read" command args
+        args = Namespace(cargs='home', command='read', facebook=False, media='', number=10, status='', twitter=True, verbose=False)
+        self.t = Twitter(args)
         print 'setUp()'
 
     def test_login(self):
@@ -17,19 +26,26 @@ class TestTwitter(unittest.TestCase):
         and first_time_auth() by checking the access
         token of 'self.auth'
         '''
-        print 'test_login()'
+        self.t.login()
+        
+        # read in access token from 'twitter-access-token.txt'
+        with open('twitter-access-token.txt', 'r') as infile:
+            content = infile.readlines()
+        access_token = content[0].rstrip()
+        access_token_secret = content[1].rstrip()
+        self.assertEqual(self.t.auth.access_token, access_token)
+        self.assertEqual(self.t.auth.access_token_secret, access_token_secret)
 
-    def test_get_tweepy_API(self):
+    #def test_get_tweepy_API(self):
         '''
         Tests get_tweepy_API & verify() indirectly
         '''
-        print 'test_get_tweepy_API()'
 
-    def test_statuses_to_tweets(self):
-        print 'test_statuses_to_tweets()'
+    #def test_statuses_to_tweets(self):
 
-    def test_post(self):
-        print 'test_post()'
+    #def test_post(self):
 
-    def test_delete(self):
-        print 'test_delete()'
+    #def test_delete(self):
+
+if __name__ == '__main__':
+    unittest.main()
