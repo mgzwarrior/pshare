6ef1168e6f21c6f4aac127c67788c6824ff67200
Brandon Powers <brandonkpowers@gmail.com>
2017-01-16 10:04:53 -0500

    Added unit test for statuses_to_tweets()

--- src/test_twitter.py
+++ src/test_twitter.py
@@ -7,6 +7,7 @@ Purpose:
 
 import unittest
 import tweepy
+import tweet
 from twitter import Twitter
 from argparse import Namespace
 
@@ -19,7 +20,6 @@ class TestTwitter(unittest.TestCase):
         # simulate "psh -t read" command args
         args = Namespace(cargs='home', command='read', facebook=False, media='', number=10, status='', twitter=True, verbose=False)
         self.t = Twitter(args)
-        print 'setUp()'
 
     def test_login(self):
         '''
@@ -46,7 +46,16 @@ class TestTwitter(unittest.TestCase):
         self.t.get_tweepy_API()
         self.assertTrue(isinstance(self.t.api, tweepy.API))
 
-    #def test_statuses_to_tweets(self):
+    def test_statuses_to_tweets(self):
+        self.t.login()
+        self.t.get_tweepy_API()
+        statuses = self.t.api.home_timeline(count=self.t.args.number)
+        tweets = self.t.statuses_to_tweets(statuses)
+        # check first element, assume the rest are the same
+        self.assertTrue(isinstance(tweets[0], tweet.Tweet))
+
+        tweets = self.t.statuses_to_tweets([])
+        self.assertFalse(tweets)
 
     #def test_post(self):
 
